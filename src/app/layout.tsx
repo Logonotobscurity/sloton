45118162a3c40cdca5ce94d09ed9e6e07ba5e133
Logo <logonthepage@gmail.com>
2025-09-01 05:07:21 +0000

    Below is the Google tag for this account. Copy and paste it in the code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -149,12 +149,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K7VDS354');`}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-0WP14XX9EB" 
+          strategy="afterInteractive" 
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0WP14XX9EB');
+          `}
         </Script>
         <script
           type="application/ld+json"
@@ -172,8 +177,6 @@ export default function RootLayout({
           anton.variable,
         )}
       >
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7VDS354"
-        height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
