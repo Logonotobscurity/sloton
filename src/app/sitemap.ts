7876f52e89252bde42d6012964bee4147650d522
Logo <logonthepage@gmail.com>
2025-09-07 15:17:01 +0000

    Why pages aren’t indexed Pages that aren’t indexed can’t be served on Go

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -20,6 +20,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/insights',
     '/contact',
     '/ab-testing',
+    '/solutions',
   ];
 
   const staticUrls = staticPages.map((page) => ({
