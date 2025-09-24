d64adc223eee41d6725ac5ca59af5b14703e76c4
Logo <logonthepage@gmail.com>
2025-09-24 23:56:22 +0000

    Audit code and execute but first let do a walk through : const callInWin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -188,16 +188,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
-        <Script 
-          src="https://www.googletagmanager.com/gtag/js?id=G-0WP14XX9EB" 
-          strategy="afterInteractive" 
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0WP14XX9EB');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-XXXXXXX');
           `}
         </Script>
         <script
@@ -232,6 +229,14 @@ export default function RootLayout({
           abhayaLibre.variable,
         )}
       >
+          <noscript>
+            <iframe 
+              src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+              height="0" 
+              width="0" 
+              style={{display: 'none', visibility: 'hidden'}}
+            ></iframe>
+          </noscript>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
