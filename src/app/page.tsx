f57d2e778163e67e2e9a3e2b1ba9c8574bdbf203
Logo <logonthepage@gmail.com>
2025-09-26 00:02:43 +0000

    move Solutions For Your Industry  below also The Technologies We Master

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,13 +21,13 @@ export default function Home() {
       <Hero />
       <StrategicPartner />
       <GeneralistApproach />
-      <TechStackCarousel />
-      <IndustriesBento />
       <TemplateLibrary />
       <ChatbotEmbed />
       <PartnershipApproach />
       <TrainingCTA />
       <Statement />
+      <IndustriesBento />
+      <TechStackCarousel />
       <FeaturedInsights />
       <Faq />
       <BottomCta />
