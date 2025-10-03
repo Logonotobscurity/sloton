ed4f526ce4ccd27dc4f77528f2fdf492c7b1de3f
Logo <logonthepage@gmail.com>
2025-10-03 10:02:36 +0000

    Audit overlapping teext in cta and refacto to reflow and fix accross oth

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -73,7 +73,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn("mt-auto flex items-center p-6 pt-0", className)}
     {...props}
   />
 ))
