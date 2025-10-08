4dc0b07568d5b4a420498bb2496740cba094892a
Quinnet Ololade <quinnetololade@gmail.com>
2025-10-08 03:02:02 +0000

    reposition to the extreme end  (_for element svg_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,10 +8,13 @@ import { Logo } from "./header/logo";
 import { cn } from "@/lib/utils";
 import React from "react";
 import { useWindowHeight } from "@/hooks/use-mobile";
+import { MobileNav } from "./header/mobile-nav";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export function Header() {
   const [isScrolled, setIsScrolled] = React.useState(false);
   const windowHeight = useWindowHeight();
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
 
   React.useEffect(() => {
     const handleScroll = () => {
@@ -31,20 +34,22 @@ export function Header() {
         isScrolled ? "bg-background/80 backdrop-blur-lg shadow-lg" : ""
       )}>
         <div className={cn(
-          "container mx-auto flex items-center transition-all duration-300 ease-in-out",
+          "container mx-auto flex items-center justify-between",
            isScrolled ? "max-w-6xl h-16" : "max-w-4xl h-24"
         )}>
-          <div className="flex-1 flex justify-start">
+          <div className="flex items-center">
              <Link href="/">
                 <Logo />
             </Link>
           </div>
 
-          <div className="flex-1 flex justify-center">
+          <div className="flex-1 lg:flex justify-center hidden">
             <MainNav />
           </div>
 
-          <div className="flex-1" />
+          <div className="flex items-center justify-end lg:hidden">
+            <MobileNav />
+          </div>
         </div>
       </header>
     </>
--- src/components/header/main-nav.tsx
+++ src/components/header/main-nav.tsx
@@ -8,11 +8,9 @@ import { Button } from "../ui/button";
 import { Menu } from "lucide-react";
 
 export function MainNav() {
-  const isDesktop = useMediaQuery("(min-width: 1024px)");
-
   return (
-    <nav aria-label="Main navigation" className="flex-1 flex justify-end lg:justify-center">
-        {isDesktop ? <DesktopNav /> : <MobileNav />}
+    <nav aria-label="Main navigation">
+        <DesktopNav />
     </nav>
   );
 }
